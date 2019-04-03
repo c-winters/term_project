4cbf1e98520cb8697c592bdcd14b2e76572ff828
c-winters <curtis.winters@hotmail.com>
2019-04-03 15:15:06 -0400

    some styles and js to use the json file

--- Project/scripts/products.js
+++ Project/scripts/products.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
+    var products;
     $.getJSON("products.json", function(data){
         $.each(data, function() {
-            var products = data.Products;
-            console.log(products);
+            $.each(this, function(key, value){
+                var p = value;
+                $("#products").append(
+                    "<div class=\"card\" id=\"product-card\">" +
+                        "<div class=\"card-body\">" +
+                            "Name: " + p.name + "<br>" +
+                            "Category: " + p.category + "<br>" +
+                            "Stock ID: #" + p.stockNumber + "<br>" +
+                            "Quantity on hand: " + p.quantityOnHand + "kg<br>" +
+                            "Price: $" + p.price +
+                        "</div>" +
+                    "</div>"
+                )
+            });
+            products = data.Products;
+            //console.log(products);
         });
     });
 });
